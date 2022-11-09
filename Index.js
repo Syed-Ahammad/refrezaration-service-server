6cf1ce98dce6d652547f3cd5292cd66c07820765
syed-ahammad <Syed_Ahammad@outlook.com>
2022-11-09 17:50:04 +0600

    MongoDB api added

--- Index.js
+++ Index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 
 const app = express();
@@ -10,9 +12,38 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/', (req, res)=>{
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.14mosb4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+      const servicesCollection = client.db("refrigaration").collection("services");
+    
+      
+    // api for get all services
+      app.get('/services', async (req, res)=>{
+          // Query for SERVICES that has 
+        const query = {};
+     
+      const cursor =  servicesCollection.find(query);
+      // since this method returns the matched document, not a cursor, print it directly
+      const services = await cursor.limit(3).toArray()
+      res.send(services);
+    });
+    
+    } finally {
+      
+    }
+  }
+  run()
+  .catch(err => console.error(err));
+
+  app.get('/', (req, res)=>{
     res.send("Server Show in browser");
 });
+
 
 
 app.listen(Port, ()=>{
